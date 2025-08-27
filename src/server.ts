cd414cfcedae1a0223334f9a724ba45c4638b56e
not-sami-01 <devilsami0101@gmail.com>
2025-08-27 21:06:29 +0500

    Renamed

--- src/server.ts
+++ src/server.ts
@@ -84,8 +84,6 @@ app.post("/api/pdf-splitter", upload.single("file"), pdfSplitterHandler);
 app.post("/api/image-to-pdf", imageToPdf);
 
 // Listener function
-// app.listen(PORT, () => {
-//   console.log(`The app is listening on http://localhost:${PORT}`);
-// });
-
-export default app;
+app.listen(PORT, () => {
+  console.log(`The app is listening on http://localhost:${PORT}`);
+});
